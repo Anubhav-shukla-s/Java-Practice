375e206d6848e50aaee3d642798b53689ca0c407
Anubhav Shukla <anubhav77175@gmail.com>
2025-12-17 21:50:00 +0530

    Multiplication table FOR loop

---
+++ Loops/formulti.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class formulti {
+    public static void main(String[] args) {
+        System.out.println("Hello from Main");
+
+       
+        Scanner sc = new Scanner(System.in);
+        int s=sc.nextInt();
+
+        int i=1;
+        for(i=1;i<=10;i++){
+            System.out.printf("%d x %d = %d\n", s, i, s*i);
+        }       
+    }
+}
