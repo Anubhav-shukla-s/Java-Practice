2d5d822de5b37796b02cf9365c0e31145359362d
Anubhav Shukla <anubhav77175@gmail.com>
2025-12-18 11:06:31 +0530

    Upside star pattern

---
+++ Loops/star.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class star {
+    public static void main(String[] args) {
+        System.out.println("Hello from Main");
+
+       
+        var sc= new Scanner(System.in);
+        int s=sc.nextInt();
+
+        int i=5;
+        for(i=5;i>=1;i--)
+            {
+
+            for(int j=0;j<i;j++)
+                {
+            System.out.print("* ");
+        } System.out.println();
+    }
+    }
+}
