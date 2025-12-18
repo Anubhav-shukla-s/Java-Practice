0a420b6540eea88fd2b643eb15c85da1f6df5797
Anubhav Shukla <anubhav77175@gmail.com>
2025-12-18 11:16:18 +0530

    Sum of first n even numbers

---
+++ Loops/firstevensum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class firstevensum {
+    public static void main(String[] args) {
+        System.out.println("Hello from Main");
+
+       
+        var sc= new Scanner(System.in);
+        int s=sc.nextInt();
+
+        int i=5;
+        int a=0;
+        while(i<=s)
+        {
+            if(i%2==0){
+                a=a+i;
+            }
+            i++;
+        }
+        System.out.printf("The sum is %d",a);
+    }       
+    
+}
