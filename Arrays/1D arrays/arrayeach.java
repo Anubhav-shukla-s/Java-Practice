b015b0d39e59d15cdacadd2b0a6db9aaab1285ac
Anubhav Shukla <anubhav77175@gmail.com>
2025-12-20 12:35:45 +0530

    Array using for each loop

---
+++ Arrays/1D arrays/arrayeach.java
@@ -0,0 +1,10 @@
+public class arrayeach {
+    public static void main(String[] args) {
+        System.out.println("Hello from Main");
+
+        int [] arr= {10, 20, 30, 40, 50};
+        for(int element : arr){
+            System.out.print(element + " ");
+        }
+    }
+}
