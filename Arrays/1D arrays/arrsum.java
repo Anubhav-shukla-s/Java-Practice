c88dd7b1ec25b134ed9b6256bc75b0f36b3dcca5
Anubhav Shukla <anubhav77175@gmail.com>
2025-12-20 12:39:31 +0530

    Sum of Array

---
+++ Arrays/1D arrays/arrsum.java
@@ -0,0 +1,14 @@
+public class arrsum {
+    public static void main(String[] args) {
+        System.out.println("Hello from Main");
+        int a=0;
+        int i=0;
+        int [] arr={10,20,30,40,50};
+        for(i=0;i<arr.length;i++)
+        {
+            a+=arr[i];
+        }
+        System.out.println(a);
+        
+    }
+}
