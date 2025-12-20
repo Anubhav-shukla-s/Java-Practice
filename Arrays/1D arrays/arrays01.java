98ca635634c36bbfaf13085465460a42acfa7f69
Anubhav Shukla <anubhav77175@gmail.com>
2025-12-20 12:34:14 +0530

    Add initial implementation of arrays example

---
+++ Arrays/1D arrays/arrays01.java
@@ -0,0 +1,9 @@
+public class arrays {
+    public static void main(String[] args) {
+        System.out.println("Hello from Main");
+        int [] numbers = {1, 2, 3, 4, 5};
+        for(int i=0;i<numbers.length;i++){
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
