b91aea92e7e20502232598194817188ffdbd5ace
Anubhav Shukla <anubhav77175@gmail.com>
2025-12-21 16:49:26 +0530

    Minimum element in an array

---
+++ Arrays/1D arrays/minelearray06.java
@@ -0,0 +1,14 @@
+public class minelearray06 {
+    public static void main(String[] args) {
+        int [] arr = {10, 2, 3, 4, 5};
+                int min=arr[0];
+
+        for(int i=0;i<arr.length;i++){
+            if(min>arr[i])
+            {
+                min=arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
