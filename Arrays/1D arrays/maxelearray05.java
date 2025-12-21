199fbdabf9a73dc11f8ac912c5960ba3e8fdafec
Anubhav Shukla <anubhav77175@gmail.com>
2025-12-21 16:50:03 +0530

    Maximum element in an array

---
+++ Arrays/1D arrays/maxelearray05.java
@@ -0,0 +1,13 @@
+public class maxelearray05 {
+    public static void main(String[] args) {
+        int max=0;
+        int [] arr = {1, 2, 3, 4, 5};
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
