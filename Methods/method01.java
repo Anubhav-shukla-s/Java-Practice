ab7da16a734138174d1c212fed642ca47fb48f2d
Anubhav Shukla <anubhav77175@gmail.com>
2025-12-21 14:49:19 +0530

    Implement add method and main function in method01

---
+++ Methods/method01.java
@@ -0,0 +1,14 @@
+public class method01 {
+
+        static int add(int a, int b) {
+                int c=a + b;
+                return c;
+        }
+    
+    public static void main(String[] args) {
+       int sum = add(5, 10);
+         System.out.println(sum);
+
+        
+    }
+}
