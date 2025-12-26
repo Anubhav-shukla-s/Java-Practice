e4e048adfb9be0de3b87a3bfc87cf3743be1bbbe
Anubhav Shukla <anubhav77175@gmail.com>
2025-12-26 18:56:21 +0530

    Update revmulti.java

--- Loops/revmulti.java
+++ Loops/revmulti.java
@@ -1,11 +1,10 @@
 public class check {
     public static void main(String[] args) {
-        System.out.println("Hello from Main");
         int a=5;
-
         for(int i=10;i>=0;i--){
             System.out.printf("%d X %d = %d\n", a, i, a*i);
         }
 
     }
 }
+
